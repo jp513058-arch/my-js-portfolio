4235ba8c7d23ce766b71740dd8a579115f7aaffa
jp513058-arch <jp.513058@gmail.com>
2026-05-14 15:52:20 +0000

    first

---
+++ app.js
@@ -0,0 +1,7 @@
+const target = document.querySelector('#target');
+
+if(target){
+    target.innerText = "JSの読み込みに成功しました";
+    target.computedStyleMap.coler = "blue";
+    console.log("ログ：JSが正しく働きました")
+}
