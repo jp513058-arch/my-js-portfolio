dbc28efa5f4b826c35350386a525c65d402e0b20
jp513058-arch <jp.513058@gmail.com>
2026-05-15 01:06:51 +0900

    app.js

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const target = document.querySelector('#target');
 
 if(target){
     target.innerText = "JSの読み込みに成功しました";
-    target.computedStyleMap.coler = "blue";
+    target.style.coler = "blue";
     console.log("ログ：JSが正しく働きました")
 }
